b4868497fbf326af7f623b4ff63ae0676085ad50
MCillo <117599499+MCillo@users.noreply.github.com>
2023-06-11 14:34:47 -0400

    Update main-view.jsx
    uncommented movie array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,33 +4,33 @@ import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([
-        // {
-        //     id: 1,
-        //     Title: 'The Shawshank Redemption',
-        //     Description: 'Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.',
-        //     Genre: 'Drama',
-        //     Director: 'Frank Darabont',          
-        //     ImagePath: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm1690056449/?ref_=tt_ov_i',
-        //     Featured: false
-        // },
-        // {
-        //     id: 2,
-        //     Title: 'The Godfather',
-        //     Description: 'Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger.',
-        //     Genre: 'Crime',
-        //     Director: 'Francis Ford Coppola',          
-        //     ImagePath: 'https://www.imdb.com/title/tt0068646/mediaviewer/rm746868224/?ref_=tt_ov_i',
-        //     Featured: false
-        // },
-        // {
-        //     id: 3,
-        //     Title: 'The Dark Knight',
-        //     Description: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
-        //     Genre: 'Action',
-        //     Director: 'Christopher Nolan',        
-        //     ImagePath: 'https://www.imdb.com/title/tt0468569/mediaviewer/rm4023877632/?ref_=tt_ov_i',
-        //     Featured: true
-        // },
+        {
+            id: 1,
+            Title: 'The Shawshank Redemption',
+            Description: 'Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.',
+            Genre: 'Drama',
+            Director: 'Frank Darabont',          
+            ImagePath: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm1690056449/?ref_=tt_ov_i',
+            Featured: false
+        },
+        {
+            id: 2,
+            Title: 'The Godfather',
+            Description: 'Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger.',
+            Genre: 'Crime',
+            Director: 'Francis Ford Coppola',          
+            ImagePath: 'https://www.imdb.com/title/tt0068646/mediaviewer/rm746868224/?ref_=tt_ov_i',
+            Featured: false
+        },
+        {
+            id: 3,
+            Title: 'The Dark Knight',
+            Description: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+            Genre: 'Action',
+            Director: 'Christopher Nolan',        
+            ImagePath: 'https://www.imdb.com/title/tt0468569/mediaviewer/rm4023877632/?ref_=tt_ov_i',
+            Featured: true
+        },
     ]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
