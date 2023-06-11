7cec8f6379fe2a2de83fae8ef84295618552591e
MCillo <117599499+MCillo@users.noreply.github.com>
2023-06-11 15:27:59 -0400

    Update main-view.jsx
    updated code

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,27 +6,27 @@ export const MainView = () => {
     const [movies, setMovies] = useState([
         {
             id: 1,
-            title: 'The Shawshank Redemption',
-            description: 'Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.',
-            genre: 'Drama',
-            director: 'Frank Darabont',          
-            image: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm1690056449/?ref_=tt_ov_i',
+            title: "The Shawshank Redemption",
+            description: "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.",
+            genre: "Drama",
+            director: "Frank Darabont",          
+            image: "https://www.imdb.com/title/tt0111161/mediaviewer/rm1690056449/?ref_=tt_ov_i",
         },
         {
             id: 2,
-            title: 'The Godfather',
-            description: 'Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger.',
-            genre: 'Crime',
-            director: 'Francis Ford Coppola',          
-            image: 'https://www.imdb.com/title/tt0068646/mediaviewer/rm746868224/?ref_=tt_ov_i',
+            title: "The Godfather",
+            description: "Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger.",
+            genre: "Crime",
+            director: "Francis Ford Coppola",          
+            image: "https://www.imdb.com/title/tt0068646/mediaviewer/rm746868224/?ref_=tt_ov_i",
         },
         {
             id: 3,
-            title: 'The Dark Knight',
-            description: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
-            genre: 'Action',
-            director: 'Christopher Nolan',        
-            image: 'https://www.imdb.com/title/tt0468569/mediaviewer/rm4023877632/?ref_=tt_ov_i',
+            title: "The Dark Knight",
+            description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+            genre: "Action",
+            director: "Christopher Nolan",        
+            image: "https://www.imdb.com/title/tt0468569/mediaviewer/rm4023877632/?ref_=tt_ov_i",
         },
     ]);
 
@@ -52,7 +52,8 @@ export const MainView = () => {
                         setSelectedMovie(newSelectedMovie);
                     }}
                 />
-            ))}
+            ))
+            }
         </div>
     );
 };
