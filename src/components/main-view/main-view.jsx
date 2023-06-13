bfb1c325ef0aedd637835c8eeb3479dfca1bcb29
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2023-06-13 16:11:30 -0400

    Update main-view.jsx
    removed contents of movies array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,57 +3,32 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "The Shawshank Redemption",
-            description: "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.",
-            genre: "Drama",
-            director: "Frank Darabont",          
-            image: "https://originalvintagemovieposters.com/wp-content/uploads/2015/05/Shawshank-Redemption-3205LB.jpg",
-        },
-        {
-            id: 2,
-            title: "The Godfather",
-            description: "Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger.",
-            genre: "Crime",
-            director: "Francis Ford Coppola",          
-            image: "https://originalvintagemovieposters.com/wp-content/uploads/2014/01/Godfather-G-1-sheet.jpg",
-        },
-        {
-            id: 3,
-            title: "The Dark Knight",
-            description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-            genre: "Action",
-            director: "Christopher Nolan",        
-            image: "https://originalvintagemovieposters.com/wp-content/uploads/2020/03/Dark-Knight-70822-scaled.jpg",
-        },
-    ]);
+  const [movies, setMovies] = useState([]);
 
-    const [selectedMovie, setSelectedMovie] = useState(null);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
-    if (selectedMovie) {
-        return (
-            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-        );
-    }
-
-    if (movies.length === 0) {
-        return <div>The list is empty!</div>;
-    }
-
+  if (selectedMovie) {
     return (
-        <div>
-            {movies.map((movie) => (
-                <MovieCard
-                    key={movie.id}
-                    movie={movie}
-                    onMovieClick={(newSelectedMovie) => {
-                        setSelectedMovie(newSelectedMovie);
-                    }}
-                />
-            ))
-            }
-        </div>
+      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
     );
+  }
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))
+      }
+    </div>
+  );
 };
