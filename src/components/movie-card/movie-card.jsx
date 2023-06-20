0168a90adb67a635904652c01e93e662e54dd895
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2023-06-19 21:11:11 -0400

    Update movie-card.jsx
    updated code

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,20 +1,20 @@
 import PropTypes from "prop-types";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-        <div
-            onClick={() => {
-                onMovieClick(movie);
-            }}
-        >
-            {movie.title}
-        </div>
-    );
+  return (
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.Title}
+    </div>
+  );
 };
 
 MovieCard.propTypes = {
-    movie: PropTypes.shape({
-        title: PropTypes.string
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
+  movie: PropTypes.shape({
+    title: PropTypes.string
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
