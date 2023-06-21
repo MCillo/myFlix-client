d94e799dc2eb1406223617bc876c1d435b4820d4
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2023-06-21 14:57:32 -0400

    Update movie-view.jsx
    updated code,
    Configured keys to display data

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -4,23 +4,23 @@ export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
-        <img src={movie.ImagePath} />
+        <img src={movie.image} />
       </div>
       <div>
         <span>Title: </span>
-        <span>{movie.Title}</span>
+        <span>{movie.title}</span>
       </div>
       <div>
         <span>Description: </span>
-        <span>{movie.Description}</span>
+        <span>{movie.description}</span>
       </div>
       <div>
         <span>Director: </span>
-        <span>{movie.Director}</span>
+        <span>{movie.director.name}</span>
       </div>
       <div>
         <span>Genre: </span>
-        <span>{movie.Genre}</span>
+        <span>{movie.genre.name}</span>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
