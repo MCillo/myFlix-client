c35b9309f91b0d17e3a40f50c1ac11dd8d13b3f2
MCillo <117599499+MCillo@users.noreply.github.com>
2023-07-03 19:34:14 -0400

    Update main-view.jsx
    updated code

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -9,29 +9,23 @@ export const MainView = () => {
   const storedToken = localStorage.getItem("token");
   console.log("line 10: ", storedUser, storedToken);
   // From reading - error before login - loads "list is empty" with no login view showing
-  //const [user, setUser] = useState(storedUser ? storedUser : null);
-  //const [token, setToken] = useState(storedToken ? storedToken : null);
-  // Works to show login
-  const [user, setUser] = useState(null);
-  const [token, setToken] = useState(null);
-  // Trial - does not work
-  // const [user, setUser] = useState(storedUser);
-  // const [token, setToken] = useState(storedToken);
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
+  // const [user, setUser] = useState(null);
+  // const [token, setToken] = useState(null);
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
 
-  useEffect(() => {
-    console.log("line 24: ", storedUser, storedToken);
-    if (!token) return;
-    console.log("line 26: ", storedUser, storedToken);
-    fetch("https://myflixapp-765.herokuapp.com/movies", {
-      headers: { Authorization: 'Bearer ${token}' }
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("line 33: ", data, storedToken);
-        const moviesFromApi = data.map((movie) => {
-          console.log("line 34: ")
+  // Get all movies from server and set them to local state
+  async function fetchMovies() {
+    try {
+      const fetchedData = await fetch(`https://myflixapp-765.herokuapp.com/movies`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const jsonData = await fetchedData.json();
+      const movies = jsonData.map((movie) => {
           return {
             id: movie._id, // is this not the key for JSX ??
             title: movie.Title,
@@ -46,14 +40,56 @@ export const MainView = () => {
               bio: movie.Director.Bio
             }
           };
-        });
+      });
 
-        setMovies(moviesFromApi);
-      })
-      .catch((error) => {
-        console.log("line 54: ", error)
-      })
+      setMovies(movies);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    if (!token) return;
+
+    fetchMovies();
   }, [token]);
+
+
+// 
+  // useEffect(() => {
+  //   console.log("line 24: ", storedUser, storedToken);
+  //   if (!token) return;
+  //   console.log("line 26: ", storedUser, storedToken);
+  //   fetch("https://myflixapp-765.herokuapp.com/movies", {
+  //     headers: { Authorization: 'Bearer ${token}' }
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log("line 33: ", data, storedToken);
+  //       const moviesFromApi = data.map((movie) => {
+  //         console.log("line 34: ")
+  //         return {
+  //           id: movie._id, // is this not the key for JSX ??
+  //           title: movie.Title,
+  //           image: movie.ImagePath,
+  //           description: movie.Description,
+  //           genre: {
+  //             name: movie.Genre.Name,
+  //             description: movie.Genre.Description,
+  //           },
+  //           director: {
+  //             name: movie.Director.Name,
+  //             bio: movie.Director.Bio
+  //           }
+  //         };
+  //       });
+
+  //       setMovies(moviesFromApi);
+  //     })
+  //     .catch((error) => {
+  //       console.log("line 54: ", error)
+  //     })
+  // }, [token]);
 
   if (!user) {
     return (
