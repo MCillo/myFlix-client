f952ac9acaa7266a70257c9f44bb5e03ddfd77d6
MCillo <117599499+MCillo@users.noreply.github.com>
2023-07-03 23:52:03 -0400

    Update movie-view.jsx
    adjusted image size for UI

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,10 +1,11 @@
 // renders the detailed information for each movie when clicked
+import "./movie-view.scss";
 
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
-        <img src={movie.image} width="200" height="300" />
+        <img src={movie.image} width="400" height="600" />
       </div>
       <div>
         <span>Title: </span>
