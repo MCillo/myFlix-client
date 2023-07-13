3ad5e203b3b0d338ec004cc7bdc09a0962791179
MCillo <117599499+MCillo@users.noreply.github.com>
2023-07-12 20:42:15 -0400

    Update profile-view.jsx
    fixed update and delete functions

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -3,35 +3,67 @@ import { Row, Card, Col, Form, Button } from "react-bootstrap";
 import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 
-export const ProfileView = ({ user }) => {
+export const ProfileView = ({ user, token, movies, handleUpdate, onLoggedOut }) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
     const [birthday, setBirthday] = useState("");
 
+    // WORKING CODE
     const handleSubmit = (event) => {
         event.preventDefault();
+
         const data = {
             Username: username,
-            Password: password,
+            //Password: password,
             Email: email,
             Birthday: birthday
-        };
+        }
 
-        fetch("https://myflixapp-765.herokuapp.com/users/", {
+        fetch(`https://myflixapp-765.herokuapp.com/users/${user.Username}`, {
             method: "PUT",
             body: JSON.stringify(data),
             headers: {
+                Authorization: `Bearer ${token}`,
                 "Content-Type": "application/json"
             }
-        }).then((response) => {
-            if (response.ok) {
-                alert("Signup Successful");
-                window.location.reload();
-            } else {
-                alert("Signup Failed");
-            }
-        });
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    alert("Something went wrong");
+                    return false;
+                }
+            })
+            .then((user) => {
+                if (user) {
+                    alert("Updated information successfully");
+                    handleUpdate(user);
+                }
+            })
+            .catch((e) => {
+                alert(e);
+            });
+    }
+
+    const handleDelete = () => {
+        fetch(`https://myflixapp-765.herokuapp.com/users/${user.Username}`, {
+            method: "DELETE",
+            headers: { Authorization: `Bearer ${token}` }
+        })
+            .then((response) => {
+                if (response.ok) {
+                    alert("Your account has been deleted!");
+                    onLoggedOut();
+                    window.location.replace("/login");
+                } else {
+                    alert("Something went wrong");
+                }
+            })
+            .catch((e) => {
+                alert(e);
+            });
     }
 
     return (
@@ -41,6 +73,7 @@ export const ProfileView = ({ user }) => {
             <Form.Label>Username:</Form.Label>
             <Form.Control
                 type="text"
+                placeholder={user.Username}
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 required
@@ -64,7 +97,7 @@ export const ProfileView = ({ user }) => {
             <Form.Label>Email:</Form.Label>
             <Form.Control
                 type="email"
-                placeholder="name@example.com"
+                placeholder={user.Email}
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
@@ -81,7 +114,12 @@ export const ProfileView = ({ user }) => {
             />
         </Form.Group>
         
-        <Button variant="primary" type="submit">Submit</Button>
+        <Button variant="primary" type="submit">Update Account</Button>
+        <Button variant="danger" onClick={() => {
+            if (confirm("You are about to permanently delete your account.")) {
+                handleDelete();
+            }
+        }}>Delete Account</Button>
     </Form>
     </>
     )
