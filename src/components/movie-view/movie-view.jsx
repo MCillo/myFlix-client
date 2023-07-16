9882e2c89507ff85dcda4558175013999da3ef40
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2023-07-16 15:29:31 -0400

    Update movie-view.jsx
    updated layout and look

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
-import { Button, Card, Col } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import "./movie-view.scss";
 
 export const MovieView = ({ movies, user, token, handleUpdate }) => {
@@ -16,92 +16,93 @@ export const MovieView = ({ movies, user, token, handleUpdate }) => {
   const [favorite, setFavorite] = useState(user.FavoriteMovies.includes(movie.id));
 
   useEffect(() => {
-      setFavorite(user.FavoriteMovies.includes(movie.id));
+    setFavorite(user.FavoriteMovies.includes(movie.id));
   }, [movieId])
 
+
+  // Add a movie to users favorite movies array
   const addFavorite = (() => {
-      fetch(`https://myflixapp-765.herokuapp.com/users/${user.Username}/movies/${movieId}`, {
-          method: "POST",
-          headers: { Authorization: `Bearer ${token}` }
+    fetch(`https://myflixapp-765.herokuapp.com/users/${user.Username}/movies/${movieId}`, {
+      method: "POST",
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          alert("Could not add this movie to your Favorites");
+          return false;
+        }
       })
-          .then((response) => {
-              if (response.ok) {
-                  return response.json();
-              } else {
-                  alert("Could not add to Favorites");
-                  return false;
-              }
-          })
-          .then((user) => {
-              if (user) {
-                  alert("This movie has been added to your Favorites");
-                  setFavorite(true);
-                  handleUpdate(user);
-              }
-          })
-          .catch((e) => {
-              alert(e)
-          });
+      .then((user) => {
+        if (user) {
+          alert("This movie has been added to your Favorites");
+          setFavorite(true);
+          handleUpdate(user);
+        }
+      })
+      .catch((e) => {
+        alert(e)
+      });
   })
-
+  // Remove a movie from the users FavoriteMovies array
   const removeFavorite = (() => {
-      fetch(`https://myflixapp-765.herokuapp.com/users/${user.Username}/movies/${movieId}`, {
-          method: "DELETE",
-          headers: { Authorization: `Bearer ${token}` }
+    fetch(`https://myflixapp-765.herokuapp.com/users/${user.Username}/movies/${movieId}`, {
+      method: "DELETE",
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          alert("Could not remove this movie from your Favorites");
+          return false;
+        }
       })
-          .then((response) => {
-              if (response.ok) {
-                  return response.json();
-              } else {
-                  alert("Could not remove from Favorites");
-                  return false;
-              }
-          })
-          .then((user) => {
-              if (user) {
-                  alert("This movie has been deleted from your Favorites");
-                  setFavorite(false);
-                  handleUpdate(user);
-              }
-          })
-          .catch((e) => {
-              alert(e);
-          });
+      .then((user) => {
+        if (user) {
+          alert("This movie has been deleted from your Favorites");
+          setFavorite(false);
+          handleUpdate(user);
+        }
+      })
+      .catch((e) => {
+        alert(e);
+      });
   })
-
-  // Test Code Stop
-
 
   return (
-    <div>
-      <div>
-        <img src={movie.image} width="400" height="600" />
-      </div>
-      <div>
-        <span>Title: </span>
-        <span>{movie.title}</span>
-      </div>
-      <div>
-        <span>Description: </span>
-        <span>{movie.description}</span>
-      </div>
-      <div>
-        <span>Director: </span>
-        <span>{movie.director.name}</span>
-      </div>
-      <div>
-        <span>Genre: </span>
-        <span>{movie.genre.name}</span>
-      </div>
-      <Link to={`/`}>
-      <Button variant="primary" >Back</Button>
-      </Link>
-      {/* <Button variant="secondary">Like</Button> */}
+    <Container>
+      <Row>
+        <Col style={{}}></Col>
+        < Col >
+          <Card className="text-center" style={{ width: '400px' }}>
+            <Card.Img variant="top" src={movie.image} width="400" height="600" />
 
-      {/* Test Code Favorites Buttons */}
-      {favorite ?
-                    <Button className="float-end" variant="outline-danger" onClick={removeFavorite}>Remove from Favorites</Button>
-                    : <Button className="float-end" variant="outline-success" onClick={addFavorite}>Add to Favorites</Button>}
-    </div>
+            <Card.Body>
+              {/* <Card.Subtitle as="h5">Title: </Card.Subtitle> */}
+              <Card.Title as="h4">{movie.title}</Card.Title>
+              {/* <Card.Subtitle>Description: </Card.Subtitle> */}
+              <Card.Text>{movie.description}</Card.Text>
+              <Card.Subtitle as="h5">Director:</Card.Subtitle>
+              <Card.Text>{movie.director.name}</Card.Text>
+              <Card.Subtitle as="h5">Genre: </Card.Subtitle>
+              <Card.Text>{movie.genre.name}</Card.Text>
+              <Link to={`/`}>
+                <Button variant="dark" >Return to Main</Button>
+              </Link>
+              <Card.Footer>
+                {/* {favorite ? */}
+                <Button className="float-end" variant="outline-dark" onClick={removeFavorite}>Remove from Favorites</Button>
+                {/* :  */}
+                <Button className="float-end" variant="outline-dark" onClick={addFavorite}>Add to Favorites</Button>
+                {/* } */}
+              </Card.Footer>
+            </Card.Body >
+          </Card >
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container >
   );
 };
