a78d8513295c3bfb35c123fd24c41b3ba9009195
MCillo <117599499+MCillo@users.noreply.github.com>
2024-01-27 17:18:06 -0500

    Update url.js
    cleaned up unused code
    updated URL

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,5 +1,10 @@
 // API on AWS connection
-export const MyFlixUrl = 'http://34.203.234.174'
+//export const MyFlixUrl = 'http://34.203.234.174'
+//export const MyFlixUrl = 'http://localhost:1234'
+
 
 // API on Heroku
-// export const MyFlixUrl = ‘https://aidens-myflix-api.herokuapp.com/api’;
+//export const MyFlixUrl = ‘https://aidens-myflix-api.herokuapp.com/api’;
+
+// API on Heroku
+export const MyFlixUrl = 'https://myflixapp-765.herokuapp.com';
