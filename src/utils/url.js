47671a3d856fec6d430aa29c0becfe5030480e05
Michael Cillo <117599499+MCillo@users.noreply.github.com>
2024-02-13 20:14:22 -0500

    Update url.js
    updated url

--- src/utils/url.js
+++ src/utils/url.js
@@ -2,7 +2,7 @@
 //export const MyFlixUrl = 'http://34.203.234.174'
 //export const MyFlixUrl = 'http://localhost:1234'
 // export const MyFlixUrl = 'http://52.90.242.134'
-export const MyFlixUrl = 'http://MyFlix-App-Load-Balancer-754907701.us-east-1.elb.amazonaws.com'  // AWS Load Balancer
+// export const MyFlixUrl = 'http://MyFlix-App-Load-Balancer-754907701.us-east-1.elb.amazonaws.com'  // AWS Load Balancer
 
 
 
@@ -12,4 +12,4 @@ export const MyFlixUrl = 'http://MyFlix-App-Load-Balancer-754907701.us-east-1.el
 //export const MyFlixUrl = ‘https://aidens-myflix-api.herokuapp.com/api’;
 
 // API on Heroku
-//export const MyFlixUrl = 'https://myflixapp-765.herokuapp.com';75
+export const MyFlixUrl = 'https://myflixapp-765.herokuapp.com';
